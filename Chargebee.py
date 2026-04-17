56da23e4e71f3c528d8ef4ce8746611ad8e92308
Yamini P <yaminiparthiban0212@gmail.com>
2026-04-17 18:17:10 +0530

    Add files via upload

---
+++ Chargebee.py
@@ -0,0 +1,34 @@
+"""def abc():
+    print("hello")
+abc()
+
+def add(a,b):
+    print(a+b)
+add(5,7)
+
+class First:
+    a =78
+
+    def method1(self):
+        print("Method1")
+
+o1 = First()
+print(o1.a)
+o1.a = 80
+print(o1.a)
+o1.method1()
+
+
+
+class Second(First):
+    b =45
+    def medthod2(self):
+        print("Method2")
+s1 = Second()
+print(s1.b)
+print(s1.a)
+"""
+import cal as m
+m.sub(3,2)
+m.sub(2,3)
+
---
+++ datatypes.py
@@ -0,0 +1,68 @@
+"""a = 5
+b=1.1
+c="yamini"
+d=2+2j
+e=True
+print(a,b,c,d,e)
+print(type(a),type(b),type(c),type(d),type(e))
+
+
+a="Yamini"
+print(a[:])
+print(a[:4])
+print(a[::2])
+print(a[::-1])
+
+"""
+"""
+b = "Bavani Kandhan"#nak
+print(b[9:6:-1])
+print(b[-5:-8:-1])
+print(b[9:-8:-1])
+print(b[-5:6:-1])
+"""
+"""
+b = "   Bavani Kandhan  "
+print(b.strip())
+print(b.lstrip())
+print(b.rstrip())
+"""
+
+"""
+
+a = ["apple", "agg", "milk"]
+print(a)
+print(type(a))
+
+b = ("apple", "agg", "milk")
+print(b)
+print(type(b))
+
+a[0] = "bag"
+print(a)
+b[0] = "cat"
+"""
+"""
+c= {1,1.1,"j"}
+print(c)
+print(type(c))
+
+d={1:"ravi",2:"raj"}
+print(d)
+print(d[1])
+"""
+d= {"veg":{"tomato":5,"brinjal":3}}
+print(d)
+print(d["veg"])
+print(d["tomato"])
+
+
+
+
+
+
+
+
+
+
+
